87a330b527142c2a1a8b86d2ebc8823bbe6ba0c3
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-06-03 22:16:49 +0700

    learn ListView widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,197 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ImageLocalScreen(),
+      home: ListViewSeparatedScreen(),
     );
   }
 }
+
+class ListViewSeparatedScreen extends StatelessWidget {
+
+  final List<int> numberList = [3, 4, 5, 6, 7, 8, 9, 10];
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+        body: ListView.separated(
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              height: 250,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                border: Border.all(
+                    color: Colors.black
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  '${numberList[index]}',
+                  style: TextStyle(
+                      fontSize: 50
+                  ),
+                ),
+              ),
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) {
+            // return Devider();
+            return Text(
+                ' '
+            );
+          },
+          itemCount: numberList.length,
+        )
+    );
+  }
+}
+
+class ListViewBuilderScreen extends StatelessWidget {
+
+  final List<int> numberList = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              height: 250,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                border: Border.all(
+                    color: Colors.black
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  '${numberList[index]}',
+                  style: TextStyle(
+                      fontSize: 50
+                  ),
+                ),
+              ),
+            );
+          },
+        itemCount: numberList.length,
+      )
+    );
+  }
+}
+
+class DynamicScrollingScreen extends StatelessWidget {
+
+  final List<int> numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: numberList.map((number) {
+          return  Container(
+            height: 250,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              border: Border.all(
+                  color: Colors.black
+              ),
+            ),
+            child: Center(
+              child: Text(
+                '$number',
+                style: TextStyle(
+                    fontSize: 50
+                ),
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
+
+class ScrollingScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            height: 250,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              border: Border.all(
+                color: Colors.black
+              ),
+            ),
+            child: Center(
+              child: Text(
+                '1',
+                style: TextStyle(
+                  fontSize: 50
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 250,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              border: Border.all(
+                  color: Colors.black
+              ),
+            ),
+            child: Center(
+              child: Text(
+                '2',
+                style: TextStyle(
+                    fontSize: 50
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 250,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              border: Border.all(
+                  color: Colors.black
+              ),
+            ),
+            child: Center(
+              child: Text(
+                '3',
+                style: TextStyle(
+                    fontSize: 50
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 250,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              border: Border.all(
+                  color: Colors.black
+              ),
+            ),
+            child: Center(
+              child: Text(
+                '4',
+                style: TextStyle(
+                    fontSize: 50
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 
 class ImageLocalScreen extends StatelessWidget {
   @override
