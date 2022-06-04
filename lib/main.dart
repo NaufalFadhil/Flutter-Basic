71758ce983b3f356f000a9b97b7d9182844c594a
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-06-04 20:58:39 +0700

    learn responsive page from desktop layout to phone layout

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,51 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LayoutBuilderScreen(),
+      home: ResponsiveScreen(),
     );
   }
 }
+
+class ResponsiveScreen extends StatelessWidget {
+  const ResponsiveScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth < 600) {
+            return ListView(
+              children: _generateContainers(),
+            );
+          } else if (constraints.maxWidth < 900) {
+            return GridView.count(
+                crossAxisCount: 2,
+              children: _generateContainers(),
+            );
+          } else {
+            return GridView.count(
+              crossAxisCount: 6,
+              children: _generateContainers(),
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  List<Widget> _generateContainers() {
+    return List<Widget>.generate(20, (index) {
+      return Container(
+        margin: EdgeInsets.all(8),
+        color: Colors.blueGrey,
+        height: 200,
+    );
+    });
+  }
+}
+
 
 class LayoutBuilderScreen extends StatelessWidget {
 
